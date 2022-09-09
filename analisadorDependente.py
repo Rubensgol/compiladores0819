16557d4006cd957e1bfb0f5d2f9e123cfc19d910
rubensgol <aralis.silva@hotmail.com>
2022-09-09 09:19:33 -0300

    alterado controle de erro

--- analisadorDependente.py
+++ analisadorDependente.py
@@ -1,3 +1,4 @@
+from asyncio.windows_events import NULL
 from cmath import pi
 from lexico import Lexico
 from tabelaTransicaoAnalisador import tabelaTransicaoDic
@@ -13,21 +14,25 @@ class AnalisaDor:
         acaba = False
         token = self.lexico.nextToken()[0]
         while not acaba:
-            if token == '&' and self.pilha[0] == '$':
-                acaba = True
-                print("aceitou")
-            else:
-                if self.pilha[0] == token  and not token == '&':
-                    self.pilha.pop(0)
-                    token = self.lexico.nextToken()[0]
+            try:
+                if token == '&' and self.pilha[0] == '$':
+                    acaba = True
+                    print("aceitou")
                 else:
-                    if not token == self.pilha[0] and self.pilha[0].startswith('<'):
-                        try:
+                    if self.pilha[0] == token  and not token == '&':
+                        self.pilha.pop(0)
+                        token = self.lexico.nextToken()[0]
+                    else:
+                        if not token == self.pilha[0] and self.pilha[0].startswith('<'):
                             regra = tabelaTransicaoDic[self.pilha[0]][token]
                             self.pilha.pop(0)
                             self.pilha = regra + self.pilha
-                        except KeyError:
-                            print("erro sintatico")
-                    else:
-                        print('terminal não esperado')
-                        acaba = True
+                        else:
+                            raise KeyError
+            except KeyError:
+                print("erro sintatico")
+                return False
+        if (self.pilha):
+            return False
+        else:
+            return True
