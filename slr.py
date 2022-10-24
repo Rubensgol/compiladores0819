43119ddb78d4bc399552f2c9889339f3352b97ee
rubensgol <aralis.silva@hotmail.com>
2022-10-24 20:26:47 -0300

    completo

--- slr.py
+++ slr.py
@@ -11,20 +11,18 @@ class AnalisaDor:
 
     def analisa(self):
         acaba = False
-        token = self.lexico.nextToken()[0]  # type: ignore
+        token = self.lexico.nextToken()[0]
 
         while (not acaba):
             if action[self.pilha[0]][token][0] == 'shift':
                 self.pilha.insert(0, action[self.pilha[0]][token][1])
-                token = self.lexico.nextToken()[0]  # type: ignore
+                token = self.lexico.nextToken()[0] 
             elif action[self.pilha[0]][token][0] == 'reduce':
-                reduce = sorted(dic[self.pilha[0]], reverse=True)
-                estado = action[self.pilha[0]][token][1]
-                for i in reduce:
-                    if i == self.pilha.pop(0):
-                        continue
-                    else:
-                        break
+                reduceComp = action[self.pilha[0]][token][2]
+                reduce = action[self.pilha[0]][token][1]
+                for i in range(0,reduce):
+                    self.pilha.pop(0)
+                estado = dic[self.pilha[0]][reduceComp]
                 self.pilha.insert(0, estado)
             elif action[self.pilha[0]][token][0] == 'STOP':
                 print("linguagem valida")
